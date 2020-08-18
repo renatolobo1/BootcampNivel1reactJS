c512e50f0f609997c6f50cb1fe50dc6993a4a404
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-18 11:30:08 -0300

    Conceitos de estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './components/Header';
 
 function App(){
+  // const projects = ['Desenvolvimento de app', 'Front-end web'];
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAppProject(){
+    // projects.push(`Novo Projeto ${Date.now()}`);
+    setProjects([...projects,`Novo Projeto ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header title="Homepage" name="Renato Lôbo">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects" name="Matheus Lôbo">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul> 
-      </Header>
+      <Header title="Projects"/>
+         
+      <ul>
+        {projects.map(project =><li key={project}>{project}</li>)}
+      </ul>
+      <button type="button" onClick={handleAppProject}>Adicionar projeto</button> 
     </>
   
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ export default function Header({title, name, children}) {
   return (
     <header>
       <h1>{title}</h1>
-      {children}
-      <h2>{name}</h2>
       
+      <h2>{name}</h2>
+      {children}
     </header>
   );
 }
